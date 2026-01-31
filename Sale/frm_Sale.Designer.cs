98f3e855d4cc4b6ff16b045db4fe8fe7aaf844c3
Thinzar Kyaw <164594037+thinzar-kyaw07@users.noreply.github.com>
2026-01-31 11:24:29 +0630

    Add files via upload

---
+++ Sale/frm_Sale.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace POS.Sale
+{
+    partial class frm_Sale
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPayment = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.lblSaleVoucher = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTotalAmount = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblRefund = new System.Windows.Forms.Label();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.lblGrandTotal = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtPayment = new System.Windows.Forms.TextBox();
+            this.dgvSale = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSale)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.DropShadowEnabled = false;
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuNew,
+            this.mnuPayment,
+            this.mnuSave});
+            this.contextMenuStrip1.Name = "mnuNew";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(159, 100);
+            this.contextMenuStrip1.Text = "New";
+            // 
+            // mnuNew
+            // 
+            this.mnuNew.Name = "mnuNew";
+            this.mnuNew.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.mnuNew.Size = new System.Drawing.Size(158, 32);
+            this.mnuNew.Text = "New";
+            // 
+            // mnuPayment
+            // 
+            this.mnuPayment.Name = "mnuPayment";
+            this.mnuPayment.ShortcutKeys = System.Windows.Forms.Keys.F2;
+            this.mnuPayment.Size = new System.Drawing.Size(158, 32);
+            this.mnuPayment.Text = "Payment";
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.ShortcutKeys = System.Windows.Forms.Keys.F3;
+            this.mnuSave.Size = new System.Drawing.Size(158, 32);
+            this.mnuSave.Text = "Save";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripLabel2,
+            this.toolStripLabel3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(967, 38);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.AutoSize = false;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(150, 30);
+            this.toolStripLabel1.Text = "New Voucher (F1) ";
+            this.toolStripLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolStripLabel1.Click += new System.EventHandler(this.mnuNew_Click);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.AutoSize = false;
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(120, 33);
+            this.toolStripLabel2.Text = "Payment (F2)";
+            this.toolStripLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolStripLabel2.Click += new System.EventHandler(this.mnuPayment_Click);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.AutoSize = false;
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(83, 33);
+            this.toolStripLabel3.Text = "Save (F3)";
+            this.toolStripLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolStripLabel3.Click += new System.EventHandler(this.mnuSave_Click);
+            // 
+            // lblSaleVoucher
+            // 
+            this.lblSaleVoucher.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSaleVoucher.Location = new System.Drawing.Point(136, 52);
+            this.lblSaleVoucher.Name = "lblSaleVoucher";
+            this.lblSaleVoucher.Size = new System.Drawing.Size(184, 40);
+            this.lblSaleVoucher.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sale Voucher";
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDate.Location = new System.Drawing.Point(452, 63);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(164, 26);
+            this.dtpDate.TabIndex = 4;
+            this.dtpDate.ValueChanged += new System.EventHandler(this.dtpDate_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(398, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Date:";
+            // 
+            // lblTotalAmount
+            // 
+            this.lblTotalAmount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotalAmount.Location = new System.Drawing.Point(842, 146);
+            this.lblTotalAmount.Name = "lblTotalAmount";
+            this.lblTotalAmount.Size = new System.Drawing.Size(100, 23);
+            this.lblTotalAmount.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(710, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Grand Total:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(710, 308);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Payment";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(710, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Total Amount:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(710, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 20);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Tax:";
+            // 
+            // lblRefund
+            // 
+            this.lblRefund.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRefund.Location = new System.Drawing.Point(842, 361);
+            this.lblRefund.Name = "lblRefund";
+            this.lblRefund.Size = new System.Drawing.Size(100, 23);
+            this.lblRefund.TabIndex = 11;
+            // 
+            // lblTax
+            // 
+            this.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTax.Location = new System.Drawing.Point(842, 204);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(100, 23);
+            this.lblTax.TabIndex = 12;
+            // 
+            // lblGrandTotal
+            // 
+            this.lblGrandTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblGrandTotal.Location = new System.Drawing.Point(842, 253);
+            this.lblGrandTotal.Name = "lblGrandTotal";
+            this.lblGrandTotal.Size = new System.Drawing.Size(100, 23);
+            this.lblGrandTotal.TabIndex = 14;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(710, 361);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(62, 20);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "Refund";
+            // 
+            // txtPayment
+            // 
+            this.txtPayment.Location = new System.Drawing.Point(842, 308);
+            this.txtPayment.Name = "txtPayment";
+            this.txtPayment.Size = new System.Drawing.Size(100, 26);
+            this.txtPayment.TabIndex = 16;
+            this.txtPayment.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPayment_KeyPress);
+            // 
+            // dgvSale
+            // 
+            this.dgvSale.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSale.Location = new System.Drawing.Point(29, 118);
+            this.dgvSale.Name = "dgvSale";
+            this.dgvSale.RowHeadersWidth = 62;
+            this.dgvSale.RowTemplate.Height = 28;
+            this.dgvSale.Size = new System.Drawing.Size(654, 297);
+            this.dgvSale.TabIndex = 17;
+            this.dgvSale.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvSale_CellEnter);
+            this.dgvSale.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dgvSale_EditingControlShowing);
+            // 
+            // frm_Sale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 450);
+            this.Controls.Add(this.dgvSale);
+            this.Controls.Add(this.txtPayment);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.lblGrandTotal);
+            this.Controls.Add(this.lblTax);
+            this.Controls.Add(this.lblRefund);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTotalAmount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSaleVoucher);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "frm_Sale";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sale List";
+            this.Load += new System.EventHandler(this.frm_Sale_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSale)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuNew;
+        private System.Windows.Forms.ToolStripMenuItem mnuPayment;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.Label lblSaleVoucher;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTotalAmount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblRefund;
+        private System.Windows.Forms.Label lblTax;
+        private System.Windows.Forms.Label lblGrandTotal;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtPayment;
+        private System.Windows.Forms.DataGridView dgvSale;
+    }
+}
---
+++ Sale/frm_SaleList.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace POS.Sale
+{
+    partial class frm_SaleList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_SaleList));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbNew = new System.Windows.Forms.ToolStripButton();
+            this.tslLabel = new System.Windows.Forms.ToolStripSplitButton();
+            this.tsmDate = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmUserName = new System.Windows.Forms.ToolStripMenuItem();
+            this.tstSearchWith = new System.Windows.Forms.ToolStripTextBox();
+            this.tsbPrint = new System.Windows.Forms.ToolStripButton();
+            this.dgvSale = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSale)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbNew,
+            this.tslLabel,
+            this.tstSearchWith,
+            this.tsbPrint});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 35);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbNew
+            // 
+            this.tsbNew.AutoSize = false;
+            this.tsbNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbNew.Image = ((System.Drawing.Image)(resources.GetObject("tsbNew.Image")));
+            this.tsbNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbNew.Name = "tsbNew";
+            this.tsbNew.Size = new System.Drawing.Size(60, 30);
+            this.tsbNew.Text = "New";
+            this.tsbNew.Click += new System.EventHandler(this.tsbNew_Click);
+            // 
+            // tslLabel
+            // 
+            this.tslLabel.AutoSize = false;
+            this.tslLabel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tslLabel.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmDate,
+            this.tsmUserName});
+            this.tslLabel.Image = ((System.Drawing.Image)(resources.GetObject("tslLabel.Image")));
+            this.tslLabel.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tslLabel.Name = "tslLabel";
+            this.tslLabel.Size = new System.Drawing.Size(100, 30);
+            this.tslLabel.Text = "SaleDate";
+            // 
+            // tsmDate
+            // 
+            this.tsmDate.Name = "tsmDate";
+            this.tsmDate.Size = new System.Drawing.Size(270, 34);
+            this.tsmDate.Text = "SaleDate";
+            this.tsmDate.Click += new System.EventHandler(this.tsmDate_Click);
+            // 
+            // tsmUserName
+            // 
+            this.tsmUserName.Name = "tsmUserName";
+            this.tsmUserName.Size = new System.Drawing.Size(270, 34);
+            this.tsmUserName.Text = "UserName";
+            this.tsmUserName.Click += new System.EventHandler(this.tsmUserName_Click);
+            // 
+            // tstSearchWith
+            // 
+            this.tstSearchWith.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.tstSearchWith.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.tstSearchWith.AutoSize = false;
+            this.tstSearchWith.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.tstSearchWith.Name = "tstSearchWith";
+            this.tstSearchWith.Size = new System.Drawing.Size(200, 30);
+            this.tstSearchWith.TextChanged += new System.EventHandler(this.tstSearchWith_TextChanged);
+            // 
+            // tsbPrint
+            // 
+            this.tsbPrint.AutoSize = false;
+            this.tsbPrint.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbPrint.Image = ((System.Drawing.Image)(resources.GetObject("tsbPrint.Image")));
+            this.tsbPrint.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbPrint.Name = "tsbPrint";
+            this.tsbPrint.Size = new System.Drawing.Size(60, 30);
+            this.tsbPrint.Text = "Print";
+            this.tsbPrint.Click += new System.EventHandler(this.tsbPrint_Click);
+            // 
+            // dgvSale
+            // 
+            this.dgvSale.AllowUserToAddRows = false;
+            this.dgvSale.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSale.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSale.Location = new System.Drawing.Point(0, 35);
+            this.dgvSale.Name = "dgvSale";
+            this.dgvSale.RowHeadersWidth = 62;
+            this.dgvSale.RowTemplate.Height = 28;
+            this.dgvSale.Size = new System.Drawing.Size(800, 415);
+            this.dgvSale.TabIndex = 2;
+            this.dgvSale.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvSale_CellClick);
+            // 
+            // frm_SaleList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvSale);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_SaleList";
+            this.Text = "Sale List";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frm_SaleList_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSale)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbNew;
+        private System.Windows.Forms.ToolStripSplitButton tslLabel;
+        private System.Windows.Forms.ToolStripMenuItem tsmDate;
+        private System.Windows.Forms.ToolStripMenuItem tsmUserName;
+        private System.Windows.Forms.ToolStripTextBox tstSearchWith;
+        private System.Windows.Forms.ToolStripButton tsbPrint;
+        private System.Windows.Forms.DataGridView dgvSale;
+    }
+}
